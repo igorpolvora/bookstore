433c6bcbe11c94a55d94198ac6342e1788074728
Igor <igorpolvora@gmail.com>
2025-09-05 05:07:33 -0300

    add: Token Authentication

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
@@ -6,6 +8,8 @@ from product.serializers.product_serializer import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Product.objects.all().order_by("id")
